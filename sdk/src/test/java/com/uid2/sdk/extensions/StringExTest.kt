2552aeaffe4da104f8a1136900909a906f19feb6
Ian Bird <ian.bird@thetradedesk.com>
2023-03-14 11:16:11 +0000

    Implement testable Base64 support

--- sdk/src/test/java/com/uid2/sdk/extensions/StringExTest.kt
+++ sdk/src/test/java/com/uid2/sdk/extensions/StringExTest.kt
@@ -1,7 +1,10 @@
 package com.uid2.sdk.extensions
 
+import java.util.Base64
 import org.json.JSONObject
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
@@ -23,12 +26,24 @@ class StringExTest {
         // Attempt to decode the string back as JSON and convert to a map. We can then simply
         // compare it to the map we originally built.
         val parsedMap = jsonString.decodeJsonToMap()
-        Assert.assertEquals(map, parsedMap)
+        assertEquals(map, parsedMap)
     }
 
     @Test
     fun `test parsing invalid json`() {
-        Assert.assertNull("not json".decodeJsonToMap())
-        Assert.assertNull("{\"missing\": \"end bracket \"".decodeJsonToMap())
+        assertNull("not json".decodeJsonToMap())
+        assertNull("{\"missing\": \"end bracket \"".decodeJsonToMap())
+    }
+
+    @Test
+    fun `test base64 decoding`() {
+        // Encode a known input, using Java's built in Base64 encoder.
+        val input = "This is a test string"
+        val inputEncoded = Base64.getEncoder().encodeToString(input.toByteArray(Charsets.UTF_8))
+
+        // Verify that the extension method decodes the ByteArray as we expect, so that it matches the original input.
+        val inputDecoded = inputEncoded.decodeBase64()
+        assertNotNull(inputDecoded)
+        assertEquals(input, inputDecoded?.toString(Charsets.UTF_8))
     }
 }
