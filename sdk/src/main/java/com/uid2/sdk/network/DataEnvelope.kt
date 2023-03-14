2a9b2c580a941428ac7d7b392603a65d9f6d6508
Ian Bird <ian.bird@thetradedesk.com>
2023-03-14 12:59:04 +0000

    Implement support for decrypting refresh responses

---
+++ sdk/src/main/java/com/uid2/sdk/network/DataEnvelope.kt
@@ -0,0 +1,67 @@
+package com.uid2.sdk.network
+
+import com.uid2.sdk.extensions.decodeBase64
+import javax.crypto.Cipher
+import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+/**
+ * This object is responsible for decoding encrypted responses when refreshing the Identity. The type of encryption used
+ * as well as the format of the expected data can be found in the following documentation:
+ *
+ * https://github.com/IABTechLab/uid2docs/blob/main/api/v2/ref-info/encryption-decryption.md
+ */
+object DataEnvelope {
+    // The name and transformation of the encryption algorithm used.
+    private const val ALGORITHM_NAME = "AES"
+    private const val ALGORITHM_TRANSFORMATION = "AES/GCM/NoPadding"
+
+    // The length of the authentication tag, in bits.
+    private const val AUTHENTICATION_TAG_LENGTH_BITS = 128
+
+    // The length of the IV, in bytes.
+    private const val IV_LENGTH_BYTES = 12
+
+    // The number of bytes expected in the decoded payload that represents the timestamp and nonce (used in the original
+    // request.)
+    private const val PAYLOAD_TIMESTAMP_LENGTH_BYTES = 8
+    private const val PAYLOAD_NONCE_LENGTH_BYTES = 8
+
+    /**
+     * Decrypts the given data with the provided key.
+     *
+     * This relies on the format of the data matching that spec-ed in the API documentation. We assume that it's AES
+     * encrypted, and includes the IV in the first 12 bytes of the buffer.
+     */
+    fun decrypt(key: String, data: String, isRefresh: Boolean): ByteArray? {
+        // Attempt to decrypt the given data with the provided key. Both the key and data are expected to be in Base64
+        // format. If this fails, then null will be returned.
+        var payload = decryptWithCipher(key.decodeBase64(), data.decodeBase64()) ?: return null
+
+        // If we are not refreshing, we expect the decoded payload to include both Timestamp and Nonce values.
+        if (!isRefresh) {
+            payload = payload.copyOfRange(
+                PAYLOAD_TIMESTAMP_LENGTH_BYTES + PAYLOAD_NONCE_LENGTH_BYTES,
+                payload.size
+            )
+        }
+
+        return payload
+    }
+
+    private fun decryptWithCipher(key: ByteArray?, data: ByteArray?): ByteArray? {
+        key ?: return null
+        data ?: return null
+
+        val secret = SecretKeySpec(key, ALGORITHM_NAME)
+        val spec = GCMParameterSpec(AUTHENTICATION_TAG_LENGTH_BITS, data, 0, IV_LENGTH_BYTES)
+
+        // Initialise the appropriate AES Cipher.
+        val cipher = Cipher.getInstance(ALGORITHM_TRANSFORMATION)?.apply {
+            init(Cipher.DECRYPT_MODE, secret, spec)
+        } ?: return null
+
+        // Decrypt the data, skipping the first 12 bytes since that contains our IV.
+        return cipher.doFinal(data, IV_LENGTH_BYTES, data.size - IV_LENGTH_BYTES)
+    }
+}
--- sdk/src/test/java/com/uid2/sdk/data/TestData.kt
+++ sdk/src/test/java/com/uid2/sdk/data/TestData.kt
@@ -1,5 +1,8 @@
 package com.uid2.sdk.data
 
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+
 object TestData {
     // A valid json representation, but missing the required "refresh_response_key" parameter.
     const val INVALID_IDENTITY = "{\"advertising_token\":\"token\",\"refresh_token\":\"refresh\",\"identity_expires\":123456,\"refresh_from\":321,\"refresh_expires\":654321}"
@@ -22,4 +25,44 @@ object TestData {
 
     const val VALID_REFRESH_OPT_OUT = "{\"status\":\"optout\",\"message\":\"User opted out\"}"
     const val VALID_REFRESH_EXPIRED_TOKEN = "{\"status\":\"expired_token\",\"message\":\"Token expired\"}"
+
+    // A Base64 encrypted refresh response that reports a status of "optout"
+    val REFRESH_TOKEN_OPT_OUT_ENCRYPTED = ResourceHelper.loadString("test-data/refresh-token-200-optout-encrypted.txt")
+    val REFRESH_TOKEN_OPT_OUT_DECRYPTED = ResourceHelper.loadString("test-data/refresh-token-200-optout-decrypted.json")
+    const val REFRESH_TOKEN_ENCRYPTED_OPT_OUT_KEY = "DIMOwK7qL6kNfzlkKNdFk2wfXhml75Rcu4FnTFnODo8="
+
+    val REFRESH_TOKEN_SUCCESS_ENCRYPTED = ResourceHelper.loadString("test-data/refresh-token-200-success-encrypted.txt")
+    val REFRESH_TOKEN_SUCCESS_DECRYPTED = ResourceHelper.loadString("test-data/refresh-token-200-success-decrypted.json")
+    const val REFRESH_TOKEN_ENCRYPTED_SUCCESS_KEY = "mJ8UtxUs4eUennG/h1v5pcxbtSHLSkzVZ1es4a3gLME="
 }
+
+/**
+ * A helper class to load test resources with a given name.
+ */
+private object ResourceHelper {
+    fun loadString(name: String) = loadString(ResourceHelper::class.java.classLoader, name)
+
+    fun loadString(loader: ClassLoader?, name: String): String {
+        loader ?: return ""
+
+        // We can use the provided ClassLoader to attempt to access the named resource via an InputStream.
+        return runCatching {
+            loader.getResourceAsStream(name).use {
+                loadString(it)
+            }
+        }.getOrDefault("")
+
+    }
+
+    fun loadString(inputStream: InputStream?): String {
+        inputStream ?: return ""
+
+        // Attempt to copy the data from the given InputStream into our own OutputStream.
+        val outputStream = ByteArrayOutputStream().apply {
+            inputStream.copyTo(this)
+        }
+
+        return outputStream.toString(Charsets.UTF_8.name()).trimEnd()
+    }
+}
+
