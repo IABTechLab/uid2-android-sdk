416976bdc982e8e0c3db098bb55a75f63025857c
Ian Bird <ian.bird@thetradedesk.com>
2023-06-03 07:14:17 +0100

    Fix version parsing for SDK and Plugins

--- sdk/src/main/java/com/uid2/UID2.kt
+++ sdk/src/main/java/com/uid2/UID2.kt
@@ -11,9 +11,6 @@ data class Version(val major: Int, val minor: Int, val patch: Int)
 object UID2 {
     private const val VERSION_STRING = BuildConfig.VERSION
 
-    private const val VERSION_COMPONENTS = 3
-    private val INVALID_VERSION = Version(0, 0, 0)
-
     /**
      * Gets the version of the included UID2 SDK library, in string format.
      */
@@ -22,12 +19,24 @@ object UID2 {
     /**
      * Gets the version of the included UID2 SDK library, in its individual major, minor and patch components.
      */
-    fun getVersionInfo(): Version {
-        val components = VERSION_STRING.split(".")
+    fun getVersionInfo() = VersionParser.parseVersion(VERSION_STRING)
+}
+
+object VersionParser {
+    private const val VERSION_COMPONENTS = 3
+    internal val INVALID_VERSION = Version(0, 0, 0)
+
+    fun parseVersion(original: String): Version {
+        // Remove any -SNAPSHOT postfix. These builds shouldn't be used in production, but are in tests.
+        val version = original.split("-").first()
+
+        val components = version.split(".")
         if (components.size != VERSION_COMPONENTS) {
             return INVALID_VERSION
         }
 
-        return Version(components[0].toInt(), components[0].toInt(), components[0].toInt())
+        return runCatching {
+            Version(components[0].toInt(), components[1].toInt(), components[2].toInt())
+        }.getOrDefault(INVALID_VERSION)
     }
 }
