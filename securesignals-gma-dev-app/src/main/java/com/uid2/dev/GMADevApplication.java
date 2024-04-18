cf8c61df8e87ce63e4e4d4e40ee71fd9b34d82f5
Ian Bird <ian.bird@thetradedesk.com>
2024-04-18 11:52:17 +0100

    Migrate to Version Catalog (+Bump Deps)

--- securesignals-gma-dev-app/src/main/java/com/uid2/dev/GMADevApplication.java
+++ securesignals-gma-dev-app/src/main/java/com/uid2/dev/GMADevApplication.java
@@ -1,6 +1,8 @@
 package com.uid2.dev;
 
 import android.app.Application;
+import android.util.Log;
+
 import com.uid2.UID2Manager;
 
 public class GMADevApplication extends Application {
@@ -8,7 +10,13 @@ public class GMADevApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        UID2Manager.init(getApplicationContext());
-    }
 
+        // Initialise the UID2Manager class. We will use it's DefaultNetworkSession rather than providing our own
+        // custom implementation. This can be done to allow wrapping something like OkHttp.
+        try {
+            UID2Manager.init(this.getApplicationContext());
+        } catch (Exception ex) {
+            Log.e("IMADevApplication", "Error initialising UID2Manager", ex);
+        }
+    }
 }
