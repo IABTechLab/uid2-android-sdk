d8df68eb18f0ceb076d9d374c5801dee74dd1b7d
Dave Snabel-Caunt <dave.snabel-caunt@thetradedesk.com>
2024-08-28 09:06:34 +0100

    Make EUIDManager an object

--- sdk/src/main/java/com/uid2/EUIDManager.kt
+++ sdk/src/main/java/com/uid2/EUIDManager.kt
@@ -12,7 +12,7 @@ import com.uid2.utils.Logger
 import com.uid2.utils.TimeUtils
 import kotlinx.coroutines.Dispatchers
 
-public class EUIDManager private constructor() {
+public object EUIDManager {
 
     public sealed interface Environment {
         public val serverUrl: String
@@ -39,84 +39,81 @@ public class EUIDManager private constructor() {
         ) : Environment
     }
 
-    public companion object {
+    // The default API server.
+    internal const val EUID_API_URL_PRODUCTION = "https://prod.euid.eu/v2"
 
-        // The default API server.
-        internal const val EUID_API_URL_PRODUCTION = "https://prod.euid.eu/v2"
+    private var serverUrl: String = EUID_API_URL_PRODUCTION
+    private var applicationId: String = APPLICATION_ID_DEFAULT
+    private var networkSession: NetworkSession = DefaultNetworkSession()
+    private var storageManager: StorageManager? = null
+    private var isLoggingEnabled: Boolean = false
 
-        private var serverUrl: String = EUID_API_URL_PRODUCTION
-        private var applicationId: String = APPLICATION_ID_DEFAULT
-        private var networkSession: NetworkSession = DefaultNetworkSession()
-        private var storageManager: StorageManager? = null
-        private var isLoggingEnabled: Boolean = false
+    private var instance: UID2Manager? = null
 
-        private var instance: UID2Manager? = null
-
-        /**
-         * Initializes the class with the given [Context], along with a [NetworkSession] that will be responsible
-         * for making any required network calls.
-         *
-         * @param context The context to initialise from. This will be used to obtain the package's metadata to extract
-         * the API URL.
-         * @param environment The API Environment to use.
-         * @param networkSession A custom [NetworkSession] which can be used for making any required network calls.
-         * The default implementation supported by the SDK can be found as [DefaultNetworkSession].
-         */
-        @JvmStatic
-        @JvmOverloads
-        @Throws(InitializationException::class)
-        public fun init(
-            context: Context,
-            environment: Environment = Environment.Production,
-            networkSession: NetworkSession = DefaultNetworkSession(),
-            isLoggingEnabled: Boolean = false,
-        ) {
-            if (instance != null) {
-                throw InitializationException()
-            }
-
-            this.serverUrl = environment.serverUrl
-            this.applicationId = context.packageName
-            this.networkSession = networkSession
-            this.storageManager = FileStorageManager(context.applicationContext, EUID)
-            this.isLoggingEnabled = isLoggingEnabled
+    /**
+     * Initializes the class with the given [Context], along with a [NetworkSession] that will be responsible
+     * for making any required network calls.
+     *
+     * @param context The context to initialise from. This will be used to obtain the package's metadata to extract
+     * the API URL.
+     * @param environment The API Environment to use.
+     * @param networkSession A custom [NetworkSession] which can be used for making any required network calls.
+     * The default implementation supported by the SDK can be found as [DefaultNetworkSession].
+     */
+    @JvmStatic
+    @JvmOverloads
+    @Throws(InitializationException::class)
+    public fun init(
+        context: Context,
+        environment: Environment = Environment.Production,
+        networkSession: NetworkSession = DefaultNetworkSession(),
+        isLoggingEnabled: Boolean = false,
+    ) {
+        if (instance != null) {
+            throw InitializationException()
         }
 
-        /**
-         * Returns True if the manager is already initialised, otherwise False.
-         */
-        @JvmStatic
-        public fun isInitialized(): Boolean = instance != null
+        this.serverUrl = environment.serverUrl
+        this.applicationId = context.packageName
+        this.networkSession = networkSession
+        this.storageManager = FileStorageManager(context.applicationContext, EUID)
+        this.isLoggingEnabled = isLoggingEnabled
+    }
 
-        /**
-         * Gets the current singleton instance of the manager.
-         *
-         * @throws InitializationException Thrown if the manager has not yet been initialised.
-         */
-        @JvmStatic
-        public fun getInstance(): UID2Manager {
-            if (storageManager == null) {
-                throw InitializationException()
-            }
-            val storage = storageManager ?: throw InitializationException()
-            val logger = Logger(isLoggingEnabled)
+    /**
+     * Returns True if the manager is already initialised, otherwise False.
+     */
+    @JvmStatic
+    public fun isInitialized(): Boolean = instance != null
 
-            return instance ?: UID2Manager(
-                UID2Client(
-                    apiUrl = serverUrl,
-                    session = networkSession,
-                    applicationId = applicationId,
-                    logger = logger,
-                ),
-                storage,
-                TimeUtils,
-                InputUtils(),
-                Dispatchers.Default,
-                true,
-                logger,
-            ).apply {
-                instance = this
-            }
+    /**
+     * Gets the current singleton instance of the manager.
+     *
+     * @throws InitializationException Thrown if the manager has not yet been initialised.
+     */
+    @JvmStatic
+    public fun getInstance(): UID2Manager {
+        if (storageManager == null) {
+            throw InitializationException()
+        }
+        val storage = storageManager ?: throw InitializationException()
+        val logger = Logger(isLoggingEnabled)
+
+        return instance ?: UID2Manager(
+            UID2Client(
+                apiUrl = serverUrl,
+                session = networkSession,
+                applicationId = applicationId,
+                logger = logger,
+            ),
+            storage,
+            TimeUtils,
+            InputUtils(),
+            Dispatchers.Default,
+            true,
+            logger,
+        ).apply {
+            instance = this
         }
     }
 }
